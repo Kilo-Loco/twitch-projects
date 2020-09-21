9ff5301fc4e5e01e459c84e2cc16a5e8f006a30d
Kilo Loco <kiloloco@me.com>
2020-09-21 14:04:32 -0700

    initial commit

---
+++ playing-with-swiftui/playing-with-swiftui/playing_with_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  playing_with_swiftuiApp.swift
+//  playing-with-swiftui
+//
+//  Created by Kyle Lee on 9/21/20.
+//
+
+import SwiftUI
+
+@main
+struct playing_with_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
